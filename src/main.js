6b5a7563a55620fc27fef454a39569aeba70b192
ShroXd <ShroXdF@gmail.com>
2020-09-20 18:14:55 +0800

    add codemirror

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "./plugins/nedb";
 import "./plugins/elementUI";
+import "./plugins/codemirror";
 
 Vue.config.productionTip = false;
 
---
+++ src/plugins/codemirror.js
@@ -0,0 +1,5 @@
+import Vue from "vue";
+import VueCodemirror from "vue-codemirror/src";
+import "codemirror/lib/codemirror.css";
+
+Vue.use(VueCodemirror);
